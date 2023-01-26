88c6cfbb9089b183543c06efafcaf99889ce911d
FlorianCabley <113980069+FlorianCabley@users.noreply.github.com>
2023-01-26 13:54:43 +0100

    final commit

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
+
+    
+    
+    const task = 'Add task:'
+    const todo = 'To do'
+    const progress = 'In progress'
+    const done = 'Done'
+    
+
+        return (<body>
+            <center>To do basic list</center>
+            <div>
+                <h2>{task}</h2>
+                <input placeholder="Enter a task" type="text" name=""></input>
+                <button>+</button>
+            </div>
+            <br></br>
+            <div>
+                <h2>{todo}</h2>
+            </div>
+            <br></br>
+            <div>
+                <h2>{progress}</h2>
+            </div>
+            <br></br>
+            <div>
+                <h2>{done}</h2>
+            </div>
+        </body>
+        
+        );
+    
+    
 };
+
+
+
 
 export default TodoListBasic;
